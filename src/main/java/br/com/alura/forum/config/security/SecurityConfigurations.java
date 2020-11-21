0d1bb53c81ef4f44636fbf644280c83e16e1dbf7
Jean Marcel de Siqueira <jean.siqueira@cwi.com.br>
2020-11-21 14:37:17 -0300

    Adicionando configuração do swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -60,6 +60,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     //Configurações de recursos estáticos(js, css, imagens, etc)
     @Override
     public void configure(final WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/swagger-resources/**");
     }
 
 }
